8d98fb46dbdae91aaa9fb3a0182f7c752a0322a6
Rocio Paez <yo@rociopaez.com>
2014-07-21 19:55:29 -0500

    Implement mock-up routes

--- app.rb
+++ app.rb
@@ -1,7 +1,36 @@
 require "cuba"
+require "mote"
+require "mote/render"
+
+Cuba.plugin(Mote::Render)
+
+Cuba.use Rack::Static,
+  urls: %w[/js /css /img],
+  root: File.expand_path("./public", __dir__)
 
 Cuba.define do
   on(root) do
-    res.write("Hello World!")
+    render("index", title: "Hola")
   end
+
+  on("login") do
+    render("login", title: "Login")
+  end
+
+  on("user") do
+    render("user", title: "User")
+  end
+
+  on("inputs") do
+    render("inputs", title: "Inputs")
+  end
+
+  on("outputs") do
+    render("outputs", title: "Outputs")
+  end
+
+  on("dashboard") do
+    render("dashboard", title: "Dashboard")
+  end
+
 end
