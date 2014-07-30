d46c6879af550ca86996c39e28661f14c7602cf9
Rocio Paez <yo@rociopaez.com>
2014-07-29 22:35:41 -0500

    Remove extracted code

--- models/user.rb
+++ models/user.rb
@@ -3,15 +3,3 @@ class User < Ohm::Model
   attribute :email
   attribute :password
 end
-
-class SignUp < Scrivener
-  attr_accessor :name
-  attr_accessor :email
-  attr_accessor :password
-
-  def validate
-    assert_present :name
-    assert_email   :email
-    assert_present :password
-  end
-end
