6cad47f25ea1cd5dd17dfae0caa72accccf19e5b
Rocio Paez <yo@rociopaez.com>
2014-08-10 00:04:44 -0500

    Implement authenticattion

--- routes/guests.rb
+++ routes/guests.rb
@@ -1,7 +1,19 @@
 class Guests < Cuba
   define do
     on("login") do
-      render("login", title: "Login")
+      on post, param("user") do |params|
+        user = User.authenticate(params["email"],  params["password"])
+
+        on user do
+          authenticate(user)
+
+          res.redirect("/dashboard")
+        end
+      end
+
+      on default do
+        render("login", title: "Login")
+      end
     end
 
     on("signup") do
