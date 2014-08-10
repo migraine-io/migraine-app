401370192fef360bdb57d6f90f5fa6257a6d2045
Rocio Paez <yo@rociopaez.com>
2014-08-10 00:05:39 -0500

    Fix routes

--- routes/inputs.rb
+++ routes/inputs.rb
@@ -1,21 +1,11 @@
 class Inputs < Cuba
   define do
-    # on("inputs") do
-    #   on("new") do
-    #     render("inputs/new", title: "New Input", types: Type.all.to_a)
-    #   end
-    #
-    #   on get do
-    #     render("inputs/index", title: "Inputs", inputs: Input.all.to_a)
-    #   end
-    # end
-
     on("new") do
-      render("inputs/new", title: "New Input", types: Type.all.to_a)
+      render("inputs/new", title: "New Input", types: Type.all)
     end
 
     on get do
-      render("inputs/index", title: "Inputs", inputs: Input.all.to_a)
+      render("inputs/index", title: "Inputs", inputs: Input.all)
     end
   end
 end
