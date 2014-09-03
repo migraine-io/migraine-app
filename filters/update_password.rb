f8f4320ae49373bd272562e0fe1c256af05f6bbc
Rocio Paez <yo@rociopaez.com>
2014-09-03 18:16:51 -0500

    Fix password confirmation

--- filters/update_password.rb
+++ filters/update_password.rb
@@ -1,13 +1,11 @@
 class UpdatePassword < Scrivener
-
   attr_accessor :password
   attr_accessor :password_confirmation
 
   def validate
-
     assert_present :password
     assert_present :password_confirmation
-    assert_equal   :password, :password_confirmation
+    assert password == password_confirmation, [:password, :not_confirmed]
     assert password.length > 5, [:password, :too_small]
   end
 end
