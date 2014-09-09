67a1d76e93efb7e6259ea62e5d43411fbd8c491b
Rocio Paez <yo@rociopaez.com>
2014-09-09 16:42:10 -0500

    Move template: types/edit  => types

--- routes/types.rb
+++ routes/types.rb
@@ -4,6 +4,10 @@ class Types < Cuba
       render("types/new", title: "New Type")
     end
 
+    on("edit") do
+      render("types/edit", title: "Edit Type")
+    end
+
     on post, param("type") do |params|
       create_type = CreateType.new(params)
       if create_type.valid?
@@ -12,8 +16,16 @@ class Types < Cuba
       end
     end
 
+    on("types/new") do
+      on post, params("type") do |params|
+        create_type = CreateType.new(params)
+        Type.create(category: type_attributes["category"], name: type_attributes["name"], user_id: user.id)
+      end
+
+    end
+
     on default do
-      render("types/edit", title: "Edit Types", user: current_user)
+      render("types", title: "Types", user: current_user)
     end
   end
 end
