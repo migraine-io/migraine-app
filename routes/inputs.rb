0cfacd56d5fcee822effb4f305dafeeec2512a95
Rocio Paez <yo@rociopaez.com>
2014-09-12 17:29:46 -0500

    Add New Input and save by user

--- routes/inputs.rb
+++ routes/inputs.rb
@@ -4,8 +4,18 @@ class Inputs < Cuba
       render("inputs/new", title: "New Input", types: Type.all)
     end
 
-    on get do
-      render("inputs/index", title: "Inputs", inputs: Input.all)
+    on post, param("input") do |params|
+      date = Date.new(params["date_year"].to_i, params["date_month"].to_i, params["date_day"].to_i)
+      type = ::Type[params["type_id"]]
+      input = Input.create(date: date, type: type, user: current_user)
+
+      res.redirect("/inputs")
     end
+
+    on get do
+      render("inputs/index", title: "Inputs", inputs: current_user.inputs)   #esto se pasa al index
+    end
+
+
   end
 end
