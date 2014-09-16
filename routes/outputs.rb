5b5ad1a1bfbf584cbf129ec6c653ebd49ba36011
Rocio Paez <yo@rociopaez.com>
2014-09-16 15:36:56 -0500

    Add edit outputs

--- routes/outputs.rb
+++ routes/outputs.rb
@@ -4,6 +4,21 @@ class Outputs < Cuba
       render("outputs/new", title: "Output")
     end
 
+    on(":id") do |id|
+      output = Output[id]
+
+      on post, param("output") do |params|
+        date = Date.new(params["date_year"].to_i, params["date_month"].to_i, params["date_day"].to_i)
+        output = output.update(date: date, scale: params["scale"], duration: params["duration"])
+
+        res.redirect("/outputs")
+      end
+
+      on get do
+        render("outputs/edit", title: "Edit Output", output: output)
+      end
+    end
+
     on post, param("output") do |params|
       date = Date.new(params["date_year"].to_i, params["date_month"].to_i, params["date_day"].to_i)
       output = Output.create(date: date, scale: params["scale"], duration: params["duration"], user: current_user)
