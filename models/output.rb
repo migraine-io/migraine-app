e588cfc7f60af308c34ede540d8ead4722167dfa
Rocio Paez <yo@rociopaez.com>
2014-09-17 15:09:18 -0500

    Define duration - outputs

--- models/output.rb
+++ models/output.rb
@@ -1,6 +1,18 @@
 class Output < Ohm::Model
   include Ohm::DataTypes
+
+  DURATIONS = {
+   "30" => "30 min",
+   "60" => "1 hour",
+   "120" => "2 hour",
+   "240" => "4 hour",
+   "480" => "8 hour",
+   "1440" => "all the day"
+  }
+
   reference :user, :User
+
+
 
   attribute :date, Type::Date
   attribute :duration, Type::Integer
